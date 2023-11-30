d0e8a782f2de25d0f93b07cccd6455a1708ed414
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-01 05:54:01 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common/pipes'
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe())
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Enable CORS with specific configuration
+  app.enableCors({
+    origin: 'http://localhost:3000', // Replace with your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // Global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(8080);
 }
+
 bootstrap();
