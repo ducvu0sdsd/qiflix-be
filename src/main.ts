36d42f127f8cada9c904462bc797111f6dc51f04
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-01 03:17:58 +0700

    d

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common/pipes'
+
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
