cb9a5d35d251926f79dcc364d9f6ee620b2cfcc0
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-02 02:12:34 +0700

    u

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://qiflix.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
