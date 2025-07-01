c0aa81b724df2487488a1a3334e1c1a7fd4b97c5
Vu Tien Duc <Vutienduc26122002@gmail.com>
2025-07-01 20:07:18 +0700

    update

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -3,12 +3,10 @@ import { MovieController } from './movie.controller';
 import { MovieService } from './movie.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MovieSchema } from './schema/movie.schema';
-import { UserSchema } from 'src/user/schema/watching.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Movie', schema: MovieSchema }]),
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
   ],
   controllers: [MovieController],
   providers: [MovieService],
